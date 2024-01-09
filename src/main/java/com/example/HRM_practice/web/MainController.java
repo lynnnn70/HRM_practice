ae2a4bb2df1a93e435777e3cc696bb50204ecf50
lynn chiang <lynnchiang@mitac.com.tw>
2024-01-09 17:53:20 +0800

    modify controller url

--- src/main/java/com/example/HRM_practice/web/MainController.java
+++ src/main/java/com/example/HRM_practice/web/MainController.java
@@ -1,29 +1,36 @@
-package com.example.HRM_practice.controller;
+package com.example.HRM_practice.web;
 
 import com.example.HRM_practice.model.entity.Users;
+import com.example.HRM_practice.security.CustomUsersDetailService;
+import com.example.HRM_practice.service.UsersService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 import java.security.Principal;
+import java.util.List;
 
-@RestController
+@Controller
 public class MainController {
 
+    @Autowired
+    private CustomUsersDetailService customUsersDetailService;
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
     //首頁 所有用戶皆可拜訪
     @GetMapping("/")
     public String home(){
-        return "home";
+        return "commons/home";
     }
 
     //登入頁面
     @GetMapping("/login")
     public String login(){
-        return "login";
+        return "commons/login";
     }
 
     //使用者可訪問的頁面
@@ -40,19 +47,19 @@ public class MainController {
         log.info("UserPrincipal 帳號 :{}", userPrincipal.getUserName());
         log.info("UserPrincipal 密碼 :{}", userPrincipal.getPassword());
 
-        return "user";
+        return "commons/user";
     }
 
     //管理者可訪問的頁面
     @GetMapping("/admin")
     public String admin(){
-        return "admin";
+        return "commons/admin";
     }
 
     //禁止訪問的頁面
     @GetMapping("/access-denied")
     public String accessDenied(){
-        return "accessDenied";
+        return "commons/accessDenied";
     }
 
 
